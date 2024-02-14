d362d86fbca69c277c7c09f6e78dfa805027b7c5
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-15 06:40:23 +0800

    unicafe: Exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,33 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleClick = (e) => {
+    switch (e.target.innerText) {
+      case 'good':
+        setGood(c => c + 1)
+        break;
+      case 'neutral':
+        setNeutral(c => c + 1)
+        break;
+      case 'bad':
+        setBad(c => c + 1)
+        break;
+    }
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={handleClick}>good</button>
+      <button onClick={handleClick}>neutral</button>
+      <button onClick={handleClick}>bad</button>
+
+      <h2>statistics</h2>
+      <ul>
+        <li>good {good}</li>
+        <li>neutral {neutral}</li>
+        <li>bad {bad}</li>
+      </ul>
     </div>
   )
 }
