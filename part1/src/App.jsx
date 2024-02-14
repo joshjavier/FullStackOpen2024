e59cb6e45268e954eaa8a7296edc869fcbe05509
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-15 06:18:30 +0800

    Practice setTimeout with useState and useEffect

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,17 @@
+import { useState, useEffect } from "react"
+
 const App = () => {
-  return <div>App</div>
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setCounter(c => c + 1)
+    }, 1000);
+
+    return () => clearTimeout(id)
+  }, [counter])
+
+  return <div>{counter}</div>
 }
 
 export default App
