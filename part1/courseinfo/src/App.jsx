82826a122b16bcf72c43138737dcfd128c3f40fc
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-15 05:37:02 +0800

    courseinfo: Exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -16,30 +16,37 @@ const Content = ({ parts }) => {
   )
 }
 
-const Total = ({ exercises }) => {
-  return <p>Number of exercises {exercises.reduce((sum, val) => sum + val, 0)}</p>
+const Total = ({ parts }) => {
+  return (
+    <p>Number of exercises {parts
+      .map(p => p.exercises)
+      .reduce((sum, val) => sum + val, 0)
+    }</p>
+  )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total exercises={[part1, part2, part3].map(p => p.exercises)} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
