8e9fd626222ada3a87b613935034b4819529fd65
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-16 04:54:23 +0800

    phonebook: Exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,18 +4,35 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
 
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons => [...persons, newPerson])
+    setNewName('')
+  }
+
   return (
     <div>
       <h1>Phonebook</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <label>
-          name: <input type="text" />
+          name:
+          <input
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
         </label>
         <div>
           <button>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
     </div>
   )
 }
