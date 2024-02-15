78995a014594363e41843f69983b0c0bb7a23fea
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-15 19:38:38 +0800

    Copy courseinfo directory from part1 to part2

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,56 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Part = ({ part }) => {
+  return <p>{part.name} {part.exercises}</p>
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {Array(parts.length).fill(0).map((_, i) => (
+        <Part key={parts[i].name} part={parts[i]} />
+      ))}
+    </>
+  )
+}
+
+const Total = ({ parts }) => {
+  return (
+    <p>Number of exercises {parts
+      .map(p => p.exercises)
+      .reduce((sum, val) => sum + val, 0)
+    }</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ],
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
