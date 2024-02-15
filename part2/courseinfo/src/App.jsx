b3a54e2024d8fd8de62dd52d7f9300f90a75290f
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-16 04:05:16 +0800

    courseinfo: Exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,40 +1,4 @@
-const Header = ({ course }) => {
-  return <h2>{course}</h2>
-}
-
-const Part = ({ part }) => {
-  return <p>{part.name} {part.exercises}</p>
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {Array(parts.length).fill(0).map((_, i) => (
-        <Part key={parts[i].name} part={parts[i]} />
-      ))}
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts
-    .map(p => p.exercises)
-    .reduce((sum, val) => sum + val, 0)
-
-  return (
-    <p><strong>total of {total} exercises</strong></p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
