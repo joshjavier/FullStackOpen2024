30b8e2a7457eaeb92584b8c5a2db8e8ee203d701
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-17 15:58:18 +0800

    Add Footer component

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -3,6 +3,21 @@ import noteService from './services/notes'
 import Note from './components/Note'
 import Notification from './components/Notification'
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16,
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
@@ -83,6 +98,7 @@ const App = () => {
         />
         <button>save</button>
       </form>
+      <Footer />
     </div>
   )
 }
