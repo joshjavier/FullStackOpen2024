2f585c09c4200bad2b5e2d9c2e14cf55e4615673
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-18 14:04:56 +0800

    countries: Exercise 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,6 +1,69 @@
-const App = () => {
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+const Country = ({ country }) => {
   return (
-    <div>App</div>
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>capital: {country.capital[0]}</p>
+      <p>area: {new Intl.NumberFormat('en-US').format(country.area)}</p>
+      <p><strong>languages:</strong></p>
+      <ul>
+        {Object.values(country.languages).map(language => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt={country.flags.alt} />
+    </div>
+  )
+}
+
+const App = () => {
+  const [query, setQuery] = useState('')
+  const [countries, setCountries] = useState([])
+  const [matches, setMatches] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const onSearch = (e) => {
+    e.preventDefault()
+
+    const q = query.trim().toLowerCase()
+    if (q === '') return
+
+    setMatches(countries.filter(({ name }) => name.common.toLowerCase().includes(q)))
+  }
+
+  return (
+    <div>
+      <h1 className="visually-hidden">Countries</h1>
+      <form onSubmit={onSearch}>
+        <label htmlFor="country-input">find countries</label>{" "}
+        <input
+          type="text"
+          id="country-input"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </form>
+      {matches.length === 1 ? (
+        <Country country={matches[0]} />
+      ) : matches.length <= 10 ? (
+        <ul>
+          {matches.map(({ name }) => name.common).map(name => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
+      ) : matches.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : null}
+    </div>
   )
 }
 
