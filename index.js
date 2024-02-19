80e4283d502b51f6a33871d5316fb63d26605d21
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-19 22:49:38 +0800

    done: Exercise 3.8

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const morgan = require('morgan')
 const app = express()
 const port = 3001
 
-app.use(morgan('tiny'))
+morgan.token('request-body', (req) => JSON.stringify(req.body))
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
 
 let persons = [
   {
