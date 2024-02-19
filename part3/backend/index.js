814766740129b4369f6e050fad9b027abd011cd2
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-20 01:48:06 +0800

    Use PORT environment variable if available

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -91,7 +91,7 @@ app.delete('/api/notes/:id', (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
