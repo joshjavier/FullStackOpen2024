182c9abbea3372882d49a63e971dbe00830ac87c
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-19 21:25:34 +0800

    done: Exercise 3.6

--- index.js
+++ index.js
@@ -42,6 +42,17 @@ app.get('/api/persons', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number is missing'
+    })
+  } else if (persons.map(p => p.name).includes(body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
   const person = {
     id: generateId(),
     name: body.name,
