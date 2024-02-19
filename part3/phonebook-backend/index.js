f2620f6a725c7bfcdf6f32e37ebbdbf4757e8b37
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-20 07:27:31 +0800

    phonebook-backend: Exercise 3.10

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 
 morgan.token('request-body', (req) => JSON.stringify(req.body))
 
