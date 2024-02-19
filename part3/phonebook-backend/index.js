910c2f4d365d457d09168efd063662929e41d5db
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-20 07:50:16 +0800

    phonebook-backend: Exercise 3.11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,6 +8,7 @@ morgan.token('request-body', (req) => JSON.stringify(req.body))
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
+app.use(express.static('dist'))
 
 let persons = [
   {
