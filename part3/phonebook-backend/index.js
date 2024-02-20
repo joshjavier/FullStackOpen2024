61ad6ffc76da42f713474203ddc4e44549291721
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-20 20:38:57 +0800

    Update package.json
    This commit updates the npm dev script to load environment variables
    from .env.local during development. It uses Node's built-in `--env-file`
    option which is still experimental, but saves us from having to add
    dotenv to the project dependencies.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT
 
 morgan.token('request-body', (req) => JSON.stringify(req.body))
 
