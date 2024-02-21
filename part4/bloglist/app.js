f29a4dbbda0d0362588ffa97bfc18572664c3d23
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-22 06:45:05 +0800

    Move app code into app.js module

---
+++ part4/bloglist/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const blogsRouter = require('./controllers/blogs')
+
+logger.info('Connecting to the database...')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB!')
+  })
+  .catch(error => {
+    logger.error(error)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
