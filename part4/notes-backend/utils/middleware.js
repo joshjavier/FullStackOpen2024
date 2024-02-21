d5815b31b4cdb8e6f65c7ba256f1ff1c26ba08db
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-22 00:57:39 +0800

    Add middleware.js module

---
+++ part4/notes-backend/utils/middleware.js
@@ -0,0 +1,31 @@
+const logger = require('./logger')
+
+const requestLogger = (req, _res, next) => {
+  logger.info('Method:', req.method)
+  logger.info('Path:  ', req.path)
+  logger.info('Body:  ', req.body)
+  logger.info('---')
+  next()
+}
+
+const unknownEndpoint = (_req, res) => {
+  res.status(404).json({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (err, _req, res, next) => {
+  logger.error(err.message)
+
+  if (err.name === 'CastError') {
+    return res.status(400).json({ error: 'malformatted id' })
+  } else if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message })
+  }
+
+  next(err)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+}
