fa06a0747e96628ab29e65274dff92c069e8f94f
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-22 08:24:40 +0800

    Exercise 4.2: Blog List, step 2
    This commit adds middlewares for:
    - logging requests
    - handling unknown endpoints
    - handling errors

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 const blogsRouter = require('./controllers/blogs')
 
@@ -20,7 +21,11 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,27 @@
+const morgan = require('morgan')
+const logger = require('./logger')
+
+morgan.token('request-body', (req) => JSON.stringify(req.body))
+const requestLogger = morgan(':method :url :status :res[content-length] - :response-time ms :request-body')
+
+const unknownEndpoint = (req, res) => {
+  return res.status(404).json({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (err, req, res, next) => {
+  logger.error(err.message)
+
+  if (err.name === 'CastError') {
+    return res.status(400).json({ error: 'malformatted id' })
+  } else if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message })
+  }
+
+  next(err)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+}
