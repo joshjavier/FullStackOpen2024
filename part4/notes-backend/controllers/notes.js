cc19dffe5c821f7d4e5ff2cade9703b392ba6883
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-24 07:28:46 +0800

    Add tests for adding a note to the database

--- part4/notes-backend/controllers/notes.js
+++ part4/notes-backend/controllers/notes.js
@@ -29,7 +29,7 @@ notesRouter.post('/', async (req, res, next) => {
 
   try {
     const savedNote = await note.save()
-    res.json(savedNote)
+    res.status(201).json(savedNote)
   } catch (error) {
     next(error)
   }
