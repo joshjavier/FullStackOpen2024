4fab57879a7bb8e54da0ec28760b45d807872d50
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-24 09:04:47 +0800

    Set up environment for testing

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,4 +1,6 @@
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 const PORT = process.env.PORT
 
 module.exports = {
