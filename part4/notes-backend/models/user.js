029444fee3044b9eb227a109f4a2718ed4849d29
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-26 09:15:22 +0800

    Add user schema and update note schema

---
+++ part4/notes-backend/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note',
+    },
+  ],
+})
+
+userSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    delete ret._id
+    delete ret.__v
+    delete ret.passwordHash
+    return ret
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
