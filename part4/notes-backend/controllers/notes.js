5e809b2f655a81ea6ca9b43a66480093e705bbc9
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-02-27 20:56:21 +0800

    Limit creating new notes to logged in users

--- part4/notes-backend/controllers/notes.js
+++ part4/notes-backend/controllers/notes.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
@@ -25,10 +26,21 @@ notesRouter.get('/:id', async (req, res, next) => {
   }
 })
 
+const getTokenFrom = (req) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 notesRouter.post('/', async (req, res, next) => {
   const body = req.body
-
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
--- part4/notes-backend/utils/middleware.js
+++ part4/notes-backend/utils/middleware.js
@@ -21,6 +21,8 @@ const errorHandler = (err, _req, res, next) => {
     return res.status(400).json({ error: err.message })
   } else if (err.name === 'MongoServerError' && err.message.includes('E11000 duplicate key error')) {
     return res.status(400).json({ error: 'expected `username` to be unique' })
+  } else if (err.name === 'JsonWebTokenError') {
+    return res.status(400).json({ error: 'token missing or invalid' })
   }
 
   next(err)
