829801c16397f869a42fabc61062927a1b2eee12
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-03 18:18:28 +0800

    Lint and fix code style for consistency

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -6,17 +6,17 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then((blogs) => {
+      setBlogs(blogs)
+    })
   }, [])
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
   )
 }
