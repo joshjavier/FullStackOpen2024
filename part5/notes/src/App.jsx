39098c0b6485bc58bd51e12db12ba0385f831365
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-03 14:20:40 +0800

    Copy part2/notes into part5/notes

---
+++ part5/notes/src/App.jsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from 'react'
+import noteService from './services/notes'
+import Note from './components/Note'
+import Notification from './components/Notification'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16,
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes => notes.map(n => n.id !== id ? n : returnedNote))
+      })
+      .catch(error => {
+        console.log(error)
+        setErrorMessage(`Note '${note.content}' was already removed from the server`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000);
+        setNotes(notes => notes.filter(n => n.id !== id))
+      })
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes => [...notes, returnedNote])
+        setNewNote('')
+      })
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          type="text"
+          value={newNote}
+          onChange={e => setNewNote(e.target.value)}
+        />
+        <button>save</button>
+      </form>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
