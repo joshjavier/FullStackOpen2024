5b04fbc2e6abf16ee2e27d0a128588d806362f29
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-03 20:00:30 +0800

    Exercise 5.1: Blog List Frontend, step 1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react'
+
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then((blogs) => {
@@ -11,9 +18,48 @@ const App = () => {
     })
   }, [])
 
+  const onChange = (event) => {
+    if (event.target.name === 'username') {
+      setUsername(event.target.value)
+    }
+
+    if (event.target.name === 'password') {
+      setPassword(event.target.value)
+    }
+  }
+
+  const onLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+
+      {user ? (
+        <p>
+          {user.name}
+          {' '}
+          logged in
+        </p>
+      ) : (
+        <LoginForm
+          onSubmit={onLogin}
+          onChange={onChange}
+          username={username}
+          password={password}
+        />
+      )}
+
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
       ))}
