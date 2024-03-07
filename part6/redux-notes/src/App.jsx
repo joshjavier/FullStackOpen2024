d7db308c37f7d85ef4bfd3f6a4eb88ccced8bc11
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-08 07:03:56 +0800

    Move store logic into main

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,27 +1,4 @@
-import { createStore } from "redux";
-import noteReducer from "./reducers/noteReducer";
-
-const store = createStore(noteReducer)
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1,
-  },
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2,
-  },
-})
-
-const App = () => {
+const App = ({ store }) => {
   return (
     <div>
       <ul>
--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createStore } from "redux";
+import noteReducer from "./reducers/noteReducer";
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  },
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  },
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(
+    <React.StrictMode>
+      <App store={store} />
+    </React.StrictMode>,
+  )
+}
+
+renderApp()
+store.subscribe(renderApp)
