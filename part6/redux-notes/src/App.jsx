00de8b15f8feb15226566faf5387b5fbd8e9a160
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-08 07:48:22 +0800

    Add functionality to toggle importance of notes

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,9 +1,19 @@
 const App = ({ store }) => {
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id }
+    })
+  }
+
   return (
     <div>
       <ul>
         {store.getState().map(note => (
-          <li key={note.id}>
+          <li
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
             {note.content} {note.important && <strong>important</strong>}
           </li>
         ))}
