10db2dc0d605d930f2a6be292bf062c313f0123f
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-09 03:42:13 +0800

    Exercise 6.3: anecdotes, step1

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,19 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'anecdotes/voteAdded':
+      return state.map(anecdote => anecdote.id === action.payload.id ? (
+        { ...anecdote, votes: anecdote.votes + 1 }
+      ) : anecdote)
+    default:
+      return state
+  }
 }
+
+export const voteAnecdote = (id) => ({
+  type: 'anecdotes/voteAdded',
+  payload: { id },
+})
 
 export default reducer
