488b621cba2d510f972a64653f4ca1b6f1ab67d2
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-08 20:26:21 +0800

    Move note list into a separate module

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,28 +1,11 @@
-import { toggleImportanceOf } from "./reducers/noteReducer";
-import { useDispatch, useSelector } from "react-redux";
 import NewNote from "./components/NewNote";
+import Notes from "./components/Notes";
 
 const App = () => {
-  const dispatch = useDispatch()
-  const notes = useSelector(state => state)
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id))
-  }
-
   return (
     <div>
       <NewNote />
-      <ul>
-        {notes.map(note => (
-          <li
-            key={note.id}
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} {note.important && <strong>important</strong>}
-          </li>
-        ))}
-      </ul>
+      <Notes />
     </div>
   )
 }
