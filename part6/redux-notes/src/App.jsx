b583b3dc523af950985dc73249df2636638dd2da
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-08 20:07:10 +0800

    Move new note form into a separate component

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,18 +1,10 @@
-import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
+import { toggleImportanceOf } from "./reducers/noteReducer";
 import { useDispatch, useSelector } from "react-redux";
+import NewNote from "./components/NewNote";
 
 const App = () => {
   const dispatch = useDispatch()
   const notes = useSelector(state => state)
-
-  const addNote = (event) => {
-    event.preventDefault()
-
-    const content = event.target.note.value
-    event.target.note.value = ''
-
-    dispatch(createNote(content))
-  }
 
   const toggleImportance = (id) => {
     dispatch(toggleImportanceOf(id))
@@ -20,10 +12,7 @@ const App = () => {
 
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input type="text" name="note" />
-        <button>add</button>
-      </form>
+      <NewNote />
       <ul>
         {notes.map(note => (
           <li
