dc34ff2727e52e737e95a44b04e238c5e0d65465
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-10 05:56:25 +0800

    Move fetching of initial state to the backend

--- part6/redux-notes/src/reducers/noteReducer.js
+++ part6/redux-notes/src/reducers/noteReducer.js
@@ -1,23 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
-
 const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 
 const noteSlice = createSlice({
   name: 'notes',
-  initialState,
+  initialState: [],
   reducers: {
     createNote: (state, action) => {
       state.push({
@@ -35,8 +22,14 @@ const noteSlice = createSlice({
       }
       return state.map(note => note.id === id ? changedNote : note)
     },
+    appendNote: (state, action) => {
+      state.push(action.payload)
+    },
+    setNotes: (state, action) => {
+      return action.payload
+    },
   }
 })
 
-export const { createNote, toggleImportanceOf } = noteSlice.actions
+export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
 export default noteSlice.reducer
---
+++ part6/redux-notes/src/services/notes.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3000/notes'
+
+const getAll = async () => {
+  const response = await axios.get(baseURL)
+  return response.data
+}
+
+export default { getAll }
