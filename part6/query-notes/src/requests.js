92d9225a58e7082406eb94010723dc06896df812
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-10 11:25:41 +0800

    Implement creating notes with React Query

--- part6/query-notes/src/requests.js
+++ part6/query-notes/src/requests.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
+const baseURL = 'http://localhost:3000/notes'
+
 export const getNotes = () => {
   return axios.get(baseURL).then(res => res.data)
+}
+
+export const createNote = (newNote) => {
+  return axios.post(baseURL, newNote).then(res => res.data)
 }
