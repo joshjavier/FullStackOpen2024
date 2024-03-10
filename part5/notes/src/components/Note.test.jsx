5cdf7ff5419438d0f06461052eba7cad67fcb308
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-11 06:18:54 +0800

    Test clicking button when toggling importance

--- part5/notes/src/components/Note.test.jsx
+++ part5/notes/src/components/Note.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 test('renders content', () => {
@@ -11,4 +12,21 @@ test('renders content', () => {
 
   const element = screen.getByText('Component testing is done with react-testing-library')
   expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true,
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Note note={note} toggleImportance={mockHandler} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
