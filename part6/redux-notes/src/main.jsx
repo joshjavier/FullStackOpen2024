54a0d756174e596a89d5d343698f47a53e02435c
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-10 08:05:25 +0800

    Move store into its own module

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import store from './store.js'
 import App from './App.jsx'
-
-import noteReducer from "./reducers/noteReducer";
-import filterReducer from './reducers/filterReducer.js';
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  }
-})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
