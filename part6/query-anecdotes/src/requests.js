858379dbebbe952fb14f750f6184bfda81d53f1b
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-10 16:43:38 +0800

    Exercise 6.21

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -5,3 +5,8 @@ const baseURL = 'http://localhost:3001/anecdotes'
 export const getAnecdotes = () => {
   return axios.get(baseURL).then(res => res.data)
 }
+
+export const createAnecdote = (content) => {
+  const anecdote = { content, votes: 0 }
+  return axios.post(baseURL, anecdote).then(res => res.data)
+}
