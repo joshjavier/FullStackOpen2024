9c17537cc622b33c58c3b74201d1454e4dfe5bea
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-11 05:52:03 +0800

    Disable eslint errors for vitest global APIs

--- part5/notes/vite.config.js
+++ part5/notes/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    AutoImport({
+      dts: false,
+      eslintrc: { enabled: true },
+      imports: ['vitest'],
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
