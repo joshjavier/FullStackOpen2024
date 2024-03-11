f1b33b3a590363ed50345962193c29d2c21e06de
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-11 08:10:21 +0800

    Exercise 5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-  test('renders the blog title and author, but not URL and number of likes', () => {
+  beforeEach(() => {
     const blog = {
       title: 'The Best Essay',
       author: 'Paul Graham',
@@ -11,9 +12,20 @@ describe('<Blog />', () => {
     }
 
     render(<Blog blog={blog} />)
+  })
 
+  test('renders the blog title and author, but not URL and number of likes', () => {
     expect(screen.getByText('The Best Essay Paul Graham'))
     expect(screen.getByText('https://paulgraham.com/best.html')).not.toBeVisible()
     expect(screen.getByText('likes 74')).not.toBeVisible()
+  })
+
+  test('shows the URL and number of likes when show button is clicked', async () => {
+    const user = userEvent.setup()
+    const showButton = screen.getByText('show')
+    await user.click(showButton)
+
+    expect(screen.getByText('https://paulgraham.com/best.html')).toBeVisible()
+    expect(screen.getByText('likes 74')).toBeVisible()
   })
 })
