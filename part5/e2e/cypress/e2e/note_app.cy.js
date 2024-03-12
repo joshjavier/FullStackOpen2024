016a429abdfcda38fe897fe2400d4bc3fce7efc2
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-13 06:32:40 +0800

    Add e2e test for creating a new note

--- part5/e2e/cypress/e2e/note_app.cy.js
+++ part5/e2e/cypress/e2e/note_app.cy.js
@@ -20,4 +20,20 @@ describe('Note app', () => {
 
     cy.contains('Hello Matti Luukkainen!')
   })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('login').click()
+      cy.get('#input-username').type('mluukkai')
+      cy.get('#input-password').type('salainen')
+      cy.get('#button-login').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+  })
 })
