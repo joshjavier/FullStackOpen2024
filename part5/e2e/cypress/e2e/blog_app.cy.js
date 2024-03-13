436ff24fac67f3f18798cb96bd162bf5dcd5e9d3
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-13 15:56:00 +0800

    Exercise 5.17: Blog List E2E Testing, step 1

--- part5/e2e/cypress/e2e/blog_app.cy.js
+++ part5/e2e/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    cy.visit('')
+  })
+
+  it('Login form is shown', function() {
+    cy.get('input:first').parent().should('contain', 'username')
+    cy.get('input:last').parent().should('contain', 'password')
+    cy.get('button').should('contain', 'log in')
   })
 })
