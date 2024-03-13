e81739ef0bd4a9c4f97bffad54e7b0a03725350c
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-13 16:16:07 +0800

    Exercise 5.18: Blog List E2E Testing, step 2

--- part5/e2e/cypress/e2e/blog_app.cy.js
+++ part5/e2e/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,11 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, {
+      name: "Josh Javier",
+      username: "joshjavier",
+      password: "mabuhay"
+    })
     cy.visit('')
   })
 
@@ -8,5 +13,25 @@ describe('Blog app', function() {
     cy.get('input:first').parent().should('contain', 'username')
     cy.get('input:last').parent().should('contain', 'password')
     cy.get('button').should('contain', 'log in')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('username').siblings('input').type('joshjavier')
+      cy.contains('password').siblings('input').type('mabuhay')
+      cy.get('button').contains('log in').click()
+
+      cy.contains('Josh Javier logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('username').siblings('input').type('joshjavier')
+      cy.contains('password').siblings('input').type('manila')
+      cy.get('button').contains('log in').click()
+
+      cy.get('[role=alert]')
+        .should('contain', 'invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
