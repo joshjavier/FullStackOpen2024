c2d3855f16c0d3c0d1b35e6698de97a197d11763
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-14 17:01:31 +0800

    Exercise 7.6: anecdotes and hooks step3

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -2,9 +2,9 @@ import { useNavigate } from 'react-router-dom'
 import { useField } from '../hooks'
 
 const CreateNew = ({ addNew }) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const { reset: contentReset, ...content } = useField('text')
+  const { reset: authorReset, ...author } = useField('text')
+  const { reset: infoReset, ...info } = useField('text')
   const navigate = useNavigate()
 
   const onSubmit = (e) => {
@@ -19,9 +19,9 @@ const CreateNew = ({ addNew }) => {
   }
 
   const onReset = () => {
-    content.reset()
-    author.reset()
-    info.reset()
+    contentReset()
+    authorReset()
+    infoReset()
   }
 
   return (
