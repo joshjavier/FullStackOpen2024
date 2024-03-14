39ecd41f41bdffb81de35d250ba992adb4b10a29
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-14 17:42:49 +0800

    Organize hooks and components into modules

---
+++ part7/country-hook/src/hooks/index.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {})
+
+  return country
+}
