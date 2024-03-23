a44007695665a02e21aa3f9c905c79a8aa22bc60
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-23 16:24:09 +0800

    Style BlogList component

--- part7/blogapp/frontend/src/components/BlogList.jsx
+++ part7/blogapp/frontend/src/components/BlogList.jsx
@@ -1,28 +1,22 @@
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 const BlogList = () => {
   const blogs = useSelector((state) => state.blogs)
 
   const byLikes = (a, b) => b.likes - a.likes
 
-  const style = {
-    border: 'solid',
-    padding: 10,
-    borderWidth: 1,
-    marginBottom: 5,
-  }
-
   return (
-    <div>
+    <ListGroup>
       {blogs.toSorted(byLikes).map((blog) => (
-        <div key={blog.id} style={style} className="blog">
+        <ListGroup.Item key={blog.id}>
           <Link to={`/blogs/${blog.id}`}>
             {blog.title} by {blog.author}
           </Link>
-        </div>
+        </ListGroup.Item>
       ))}
-    </div>
+    </ListGroup>
   )
 }
 
