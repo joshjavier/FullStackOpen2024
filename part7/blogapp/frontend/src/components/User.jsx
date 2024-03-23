812041eb2b1672c487a26d06625a6c17aa2accbe
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-03-23 17:16:21 +0800

    Refactor data fetching logic in User component

--- part7/blogapp/frontend/src/components/User.jsx
+++ part7/blogapp/frontend/src/components/User.jsx
@@ -1,11 +1,9 @@
-import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
-import { useMatch } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 const User = () => {
-  const users = useSelector((state) => state.users)
-  const match = useMatch('/users/:id')
-  const user = match ? users.find((user) => user.id === match.params.id) : null
+  const { id } = useParams()
+  const user = useSelector(({ users }) => users.find((user) => user.id === id))
 
   if (!user) return
 
@@ -15,20 +13,13 @@ const User = () => {
       <h3>added blogs</h3>
       <ul>
         {user.blogs.map((blog) => (
-          <li key={blog.id}>{blog.title}</li>
+          <li key={blog.id}>
+            <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+          </li>
         ))}
       </ul>
     </div>
   )
-}
-
-User.propTypes = {
-  user: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    name: PropTypes.string,
-    blogs: PropTypes.array.isRequired,
-    id: PropTypes.string.isRequired,
-  }),
 }
 
 export default User
