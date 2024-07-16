8d64dc1627bc5f24858cdafc4bf827de9eca9d98
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-07-17 06:37:42 +0800

    Optimize updating cache

--- part8/apollo-client-study/src/components/PersonForm.jsx
+++ part8/apollo-client-study/src/components/PersonForm.jsx
@@ -5,7 +5,11 @@ import Field from './Field'
 
 const PersonForm = ({ setError }) => {
   const [ createPerson ] = useMutation(CREATE_PERSON, {
-    refetchQueries: [{ query: ALL_PERSONS }],
+    update: (cache, { data: { addPerson } }) => {
+      cache.updateQuery({ query: ALL_PERSONS }, ({ allPersons }) => ({
+        allPersons: allPersons.concat(addPerson)
+      }))
+    },
     onError: (error) => {
       const messages = error.graphQLErrors.map(e => e.message).join('\n')
       setError(messages)
