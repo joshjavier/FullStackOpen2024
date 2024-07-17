38a73c5848ab5f5e1669355b4dc78afe3bc15497
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-07-18 05:51:31 +0800

    Exercise 8.19: Books by genre, part 1

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react"
 import { useQuery } from "@apollo/client"
 import { ALL_BOOKS } from "../queries"
+import '../css/button.css'
 
 const Books = (props) => {
   const { loading, data } = useQuery(ALL_BOOKS)
+  const [selectedGenre, setGenre] = useState(null)
 
   if (!props.show) return
 
   if (loading) return <div>Loading...</div>
 
   const books = data.allBooks
+  const genres = new Set(books.reduce((genres, book) => genres.concat(book.genres), []))
+
+  const booksToShow = selectedGenre
+    ? books.filter(book => book.genres.includes(selectedGenre))
+    : books
 
   return (
     <div>
@@ -21,7 +29,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {booksToShow.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -30,6 +38,15 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+
+      {[...genres].map(genre => (
+        <button
+          key={genre}
+          onClick={() => setGenre(genre)}
+          aria-pressed={genre === selectedGenre}
+        >{genre}</button>
+      ))}
+      <button onClick={() => setGenre(null)}>all genres</button>
     </div>
   )
 }
