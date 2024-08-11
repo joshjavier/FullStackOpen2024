75855fe3821c9ae3638828f0bd73437d5e4787ed
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-08-11 16:48:08 +0800

    Data

---
+++ part9/flights/src/routes/diaries.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import * as diaryService from '../services/diaryService';
+
+const router = Router();
+
+router.get('/', (_req, res) => {
+  res.send(diaryService.getNonSensitiveEntries());
+});
+
+router.post('/', (_req, res) => {
+  res.send('Saving a diary!');
+});
+
+export default router;
