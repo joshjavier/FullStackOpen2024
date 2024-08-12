1aa89b8a19be1e48acefc84fe01abb3bafd9e0fc
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-08-12 13:56:21 +0800

    Extend eslint with stylistic & type-aware linting

--- part9/flights-frontend/eslint.config.js
+++ part9/flights-frontend/eslint.config.js
@@ -3,24 +3,42 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+  ],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
+    parserOptions: {
+      project: [
+        './tsconfig.node.json',
+        './tsconfig.app.json',
+      ],
+      tsconfigRootDir: import.meta.dirname,
+    },
   },
   plugins: {
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
+    '@stylistic': stylistic,
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    ...stylistic.configs['recommended-flat'].rules,
+    '@stylistic/arrow-parens': ['error', 'always'],
+    '@stylistic/jsx-one-expression-per-line': [
+      'error',
+      { allow: 'single-line' },
     ],
   },
 })
