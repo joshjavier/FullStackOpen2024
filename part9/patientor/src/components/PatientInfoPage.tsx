66530cc97a31784443ba7d3ea18d6248eddb651e
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-08-14 05:44:06 +0800

    Exercise 9.21: Patientor, step2

---
+++ part9/patientor/src/components/PatientInfoPage.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import patientService from "../services/patients";
+import { Patient } from "../types";
+import { isAxiosError } from "axios";
+import MaleIcon from "@mui/icons-material/Male";
+import FemaleIcon from "@mui/icons-material/Female";
+import TransgenderIcon from '@mui/icons-material/Transgender';
+
+const PatientInfoPage = () => {
+  const { id } = useParams();
+  const [patient, setPatient] = useState<Patient | null>(null);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchPatientInfo = async () => {
+      try {
+        const patient = await patientService.getPatient(id!);
+        setPatient(patient);
+      } catch (error) {
+        if (isAxiosError(error)) {
+          console.log('Error: ' + error.response?.data);
+          setError('Patient not found.');
+        } else {
+          console.log('Unknown error: ' + error);
+          setError('Unknown error occurred.');
+        }
+      }
+    };
+    fetchPatientInfo();
+  }, [id]);
+
+  if (error || !patient) {
+    return <p><em>{error || 'Loading...'}</em></p>;
+  }
+
+  const genderIcon = patient.gender === 'male' ? <MaleIcon />
+    : patient.gender === 'female' ? <FemaleIcon />
+    : <TransgenderIcon />;
+
+  return (
+    <div>
+      <h2>{patient.name} {genderIcon}</h2>
+      <p>ssn: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
+      <p>date of birth: {patient.dateOfBirth}</p>
+    </div>
+  );
+};
+
+export default PatientInfoPage;
